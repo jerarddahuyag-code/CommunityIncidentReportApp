40a496af5c790ab233e93b9477825bf3421d8bd1
Jerard Kent Dahuyag <jerard.dahuyag@ext.bdo.global>
2026-02-26 12:13:12 +0800

    initialize using vite

---
+++ client/src/pages/login.tsx
@@ -0,0 +1,19 @@
+function Login() {
+    return (
+        <div>
+            <p>Logo</p>
+            <div>
+                <label>Username</label>
+                <input placeholder="Username"></input>
+            </div>
+            <div>
+                <label>Password</label>
+                <input type="password" placeholder="Password"></input>
+            </div>
+            <button>Log in</button>
+            <a>Create an Account</a>
+        </div>
+    )
+}
+
+export default Login
